5294b335c5217624146e1fef4b58b54c01c7cfd8
diegiesskanne <jaromir.vogt@web.de>
2021-05-22 12:48:43 +0200

    local changes remove on merge

--- embark/entrypoint.sh
+++ embark/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# remove before commit
 export DJANGO_SETTINGS_MODULE=embark.settings
+pip3 install mysqlclient
+pip3 install -r requirements.txt
+
 uwsgi --wsgi-file /app/embark/embark/wsgi.py --http :8000 --workers=2 &
 daphne embark.asgi:application -p 8001 -b '0.0.0.0'
