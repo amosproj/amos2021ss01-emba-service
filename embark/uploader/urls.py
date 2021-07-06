1636eccc59bcc7a8439d13e304fea414006fb5e1
Maximilian Wagner <wagnermaximilian@aol.com>
2021-07-06 20:52:43 +0200

    add comma

--- embark/uploader/urls.py
+++ embark/uploader/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
 
     path('get_load/', views.get_load, name='embark-get-load'),
     path('get_individual_report/<int:analyze_id>/', views.get_individual_report, name='embark-get-individual-report'),
-    path('get_accumulated_reports/', views.get_accumulated_reports, name='embark-get-accumulated-reports')
+    path('get_accumulated_reports/', views.get_accumulated_reports, name='embark-get-accumulated-reports'),
     path('check_login/', views.check_login, name='embark-check-login')
 ]
