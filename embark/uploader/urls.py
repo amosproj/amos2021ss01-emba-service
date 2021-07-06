576ef64720c3951990ab1ab0e4bcde1edbb9383d
VAISHNAVI UMESH <vaishnavi.umesh@fau.de>
2021-07-06 16:14:03 +0200

    fixed url

--- embark/uploader/urls.py
+++ embark/uploader/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     path('download_zipped/<int:analyze_id>/', views.download_zipped, name='embark-download'),
 
     path('get_load/', views.get_load, name='embark-get-load'),
-    
+
     path('get_individual_report/<int:analyze_id>/', views.get_individual_report, name='embark-get-individual-report'),
     path('get_accumulated_reports/', views.get_accumulated_reports, name='embark-get-accumulated-reports'),
     path('check_login/', views.check_login, name='embark-check-login')
-
+]
